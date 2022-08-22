4158116a78d64bff3279966291c92d72b959f752
Bob <bob@justbetter.nl>
2022-08-22 16:03:55 +0200

    Added event to update localStorage values

--- resources/js/alerts.js
+++ resources/js/alerts.js
@@ -1,6 +1,14 @@
 document.addEventListener('turbolinks:load', () => {
-    window.app.$on('logged-in', async () => {
+    window.app.$on('logged-in', getAlerts);
+    window.app.$on('alerts-updated', getAlerts);
+    window.app.$on('logout', () => localStorage.removeItem('alerts'));
+
+    async function getAlerts() {
+        if (!localStorage.token) {
+            return;
+        }
+
         window.axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.token}`;
         await window.axios.post('/api/alerts').then((res) => localStorage.alerts = res.data)
-    });
+    }
 });
--- resources/views/subscribe-form.blade.php
+++ resources/views/subscribe-form.blade.php
@@ -1,4 +1,6 @@
-<graphql-mutation v-if="user" v-cloak query="@include('rapidez-product-alert::graphql.product-alert-subscribe')" :variables='{product_id: {{ $product_id }}, email: user.email}' :notify="{message: '@lang('We will let you know once this product is back in stock')', type: 'success'}">
+<graphql-mutation v-if="user" v-cloak query="@include('rapidez-product-alert::graphql.product-alert-subscribe')" :variables='{product_id: {{ $product_id }}, email: user.email}' :callback="() => {
+    window.app.$emit('alerts-updated')
+}" :notify="{message: '@lang('We will let you know once this product is back in stock')', type: 'success'}">
     <div slot-scope="{ mutate }">
         <x-rapidez::button v-on:click="mutate" class="flex items-center" dusk="product-alert-subscribe">
             <span class="mr-2">@lang('Notify me when this product is in stock')</span><x-heroicon-o-bell class="h-5 w-5 ml-auto"/>
