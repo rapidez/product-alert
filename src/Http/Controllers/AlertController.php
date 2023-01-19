5244f5957c201090de6b4f6cdaefe71243195078
indykoning <15870933+indykoning@users.noreply.github.com>
2023-01-19 16:57:01 +0100

    Prevent "Attempt to read customer_id on null" (#4)

--- src/Http/Controllers/AlertController.php
+++ src/Http/Controllers/AlertController.php
@@ -12,7 +12,7 @@ class AlertController extends Controller
     {
         abort_if(!$request->header('authorization'), 401);
         $customer = DB::table('oauth_token')->select('customer_id')->where('token', str_replace('Bearer ', '', $request->header('authorization')))->first();
-        abort_if(!$customer->customer_id, 401);
+        abort_if(!$customer?->customer_id, 401);
         $alerts = DB::table('product_alert_stock')->select('product_id')->where('customer_id', $customer->customer_id)->get();
 
         return $alerts->pluck('product_id')->toJson() ?? [];
