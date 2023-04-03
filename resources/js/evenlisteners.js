1c387ec01b9ccd69aec13e6702832a712b1166d5
indy koning <indy@justbetter.nl>
2023-04-03 16:59:57 +0200

    Rename to package.js, update readme

---
+++ resources/js/evenlisteners.js
@@ -0,0 +1,14 @@
+document.addEventListener('turbo:load', () => {
+    window.app.$on('logged-in', getAlerts);
+    window.app.$on('alerts-updated', getAlerts);
+    window.app.$on('logout', () => localStorage.removeItem('alerts'));
+
+    async function getAlerts() {
+        if (!localStorage.token) {
+            return;
+        }
+
+        window.axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.token}`;
+        await window.axios.post('/api/product-alerts').then((res) => localStorage.alerts = res.data)
+    }
+});
