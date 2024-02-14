7dfef19b564840e135da0e0edc63df002b3aa960
jordy <jordy@justbetter.nl>
2024-02-14 14:06:38 +0100

    Removed partials

--- resources/views/partials/alert-login.blade.php
+++
@@ -1,3 +0,0 @@
-<x-rapidez::button :href="route('account.login')" class="flex items-center" dusk="product-alert-login">
-    <span>@lang('Log in to be notified when this product is in stock')</span>
-</x-rapidez::button>
