0ba7bbedbaed5f7127591a29ccbdd50799088720
jordy <jordy@justbetter.nl>
2024-02-14 15:04:17 +0100

    included all attributes within the merge for both buttons

--- resources/views/partials/login.blade.php
+++ resources/views/partials/login.blade.php
@@ -1,4 +1,10 @@
-<x-rapidez::button :href="route('account.login')" dusk="product-alert-login" :attributes="$login->attributes->class('flex items-center justify-center w-full')">
+<x-rapidez::button
+    :attributes="$login->attributes->merge([
+        'class' => 'flex items-center justify-center w-full',
+        'dusk' => 'product-alert-login',
+        'href' => route('account.login'),
+    ])"
+>
     <x-heroicon-o-bell class="h-5 w-5 mr-1"/>
     <span>@lang('Notify me')</span>
 </x-rapidez::button>
--- resources/views/partials/subscribe.blade.php
+++ resources/views/partials/subscribe.blade.php
@@ -1,4 +1,10 @@
-<x-rapidez::button v-on:click="mutate" dusk="product-alert-subscribe" :attributes="$subscribe->attributes->class('flex items-center justify-center w-full')">
+<x-rapidez::button
+    :attributes="$subscribe->attributes->merge([
+        'class' => 'flex items-center justify-center w-full',
+        'dusk' => 'product-alert-subscribe',
+        'v-on:click' => 'mutate',
+    ])"
+>
     <x-heroicon-o-bell class="h-5 w-5 mr-1"/>
     <span>@lang('Notify me')</span>
 </x-rapidez::button>        
