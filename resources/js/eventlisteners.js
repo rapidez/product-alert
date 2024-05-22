fae7d724b20fbd6a0e8a5dc75cb47491d52dcfcb
indykoning <15870933+indykoning@users.noreply.github.com>
2024-05-22 16:12:18 +0200

    Rapidez v2 compatibility (#12)

--- resources/js/eventlisteners.js
+++ resources/js/eventlisteners.js
@@ -1,13 +1,15 @@
+import { useLocalStorage } from '@vueuse/core'
+
 document.addEventListener('turbo:load', () => {
     window.app.$on('logged-in', getAlerts);
     window.app.$on('alerts-updated', getAlerts);
-    window.app.$on('logout', () => localStorage.removeItem('alerts'));
+    window.app.$on('logout', () => useLocalStorage('alerts', []).value = null);
+
 
     async function getAlerts() {
-        if (!localStorage.token) {
-            return;
-        }
-
-        await window.axios.post(window.url('/api/product-alerts'), {}, { headers: { Authorization: window.magentoUser.defaults.headers.common['Authorization'] }}).then((res) => localStorage.alerts = res.data)
+        useLocalStorage('alerts', []).value = await window.rapidezAPI(
+            'post',
+            'product-alerts'
+        )
     }
 });
