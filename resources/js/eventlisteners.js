ed5fdce972b154e58eac043c937d0d5b9957b813
indykoning <15870933+indykoning@users.noreply.github.com>
2024-12-03 14:46:21 +0100

    Listen to vue:loaded instead (#15)

--- resources/js/eventlisteners.js
+++ resources/js/eventlisteners.js
@@ -1,6 +1,6 @@
 import { clear, refresh } from './stores/useAlerts'
 
-document.addEventListener('turbo:load', () => {
+document.addEventListener('vue:loaded', () => {
     window.app.$on('logged-in', refresh);
     window.app.$on('alerts-updated', refresh);
     window.app.$on('logout', clear);
